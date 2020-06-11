b5e4f901c1f9863c92c7047a341f8ad4f015f5b5
Harri Pahirathan <harripahirathan@gmail.com>
2020-06-10 21:19:53 -0400

    Inital Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+app.listen(8080, function(){
+    console.log("Server is running");
+});
+
+
+
+
